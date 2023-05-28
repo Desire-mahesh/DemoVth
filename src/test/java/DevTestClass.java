7eac2671be8b7ef14eb6595779ce20960ceefc28
Mahesh <mahesh4521@gmail.com>
2023-05-28 08:07:29 +0530

    commit 2

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -2,5 +2,7 @@ public class DevTestClass {
     public static void main(String[] args)
     {
         System.out.println("Dev Test Class");
+        //this is dev test
+        System.out.println("this is dev test class ..........");
     }
 }
